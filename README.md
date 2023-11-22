90d2ccff85114caae0931bf8e5b8b91359463b63
鲁湛湛 <cn_zane@outlook.com>
2023-11-22 13:51:31 +0800

    init: 初始化仓库

---
+++ README.md
@@ -0,0 +1,9 @@
+# 基于Python爬取Bing每日壁纸
+
+#### 根据bing提供的Api获取每日壁纸的下载信息
+
+#### ![image-20231122134906398](https://raw.githubusercontent.com/cn-zane/picture-hosting/master/Typora/image-20231122134906398.png)
+
+#### 下载每日壁纸到对应的月份目录下
+
+![image-20231122134919268](https://raw.githubusercontent.com/cn-zane/picture-hosting/master/Typora/image-20231122134919268.png)
---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from urllib import request
+import os
+import datetime
+import json
+
+
+def print_hi():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+    res_html = requests.get("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&uhd=1", headers=headers)
+    res = res_html.content.decode("utf-8")
+    json_res = json.loads(res)
+    url = json_res["images"][0]["url"]
+    # url截取
+    url = url[:url.find('&')];
+    picture_title = json_res["images"][0]["copyright"]
+    end_date = json_res["images"][0]["enddate"]
+    # 截取标题
+    picture_title = picture_title[:picture_title.rfind('(')].strip()
+    # 拼接url
+    url = "https://www.bing.com{}".format(url)
+    # 获取当前日期信息
+    now = datetime.datetime.now()
+    curr_year = now.year
+    curr_date = datetime.datetime.strftime(now, "%Y-%m")
+    # 保存的路径(自动创建年月目录)
+    path = r"E:\pythonProject\bing-wallpaper\download"
+    month_dir_path = path + "/{}/{}".format(curr_year, curr_date)
+    # 月份路径(不存在则创建)
+    if not os.path.exists(month_dir_path):
+        os.makedirs(month_dir_path)
+
+    # 图片下载路径
+    pic_down_load_path = month_dir_path + "/{}-{}.jpg".format(picture_title, end_date)
+    # 图片不存在->下载
+    if not os.path.exists(pic_down_load_path):
+        request.urlretrieve(url, pic_down_load_path)
+        print(picture_title, "下载成功")
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print_hi()
