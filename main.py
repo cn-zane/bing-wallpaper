388a14d4526e545fc99d31d588f5afdabafeb8f5
zane <cn_zane@outlook.com>
2023-12-01 02:00:01 +0800

    自动提交: 2023-12-01 02:00:01

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def print_hi():
     now = datetime.datetime.now()
     curr_year = now.year
     curr_date = datetime.datetime.strftime(now, "%Y-%m")
+    webp_date = datetime.datetime.strftime(now, "%Y-%m-%d")
     # 保存的路径(自动创建年月目录)
     path = "/mydata/projects/bing-wallpaper/download"
-    webp_path = "/mydata/projects/bing-wallpaper/webpPic"
+    webp_path = "/mydata/nginx/images/"
     month_dir_path = path + "/{}/{}".format(curr_year, curr_date)
-    month_dir_webp_path = webp_path + "/{}/{}".format(curr_year, curr_date)
 
     # 月份路径(不存在则创建)
     if not os.path.exists(month_dir_path):
@@ -41,8 +41,7 @@ def print_hi():
 
     # 图片下载路径
     pic_down_load_path = month_dir_path + "/{}-{}.jpg".format(picture_title, end_date)
-    webp_pic_down_load_path = month_dir_webp_path + "/{}-{}.webp".format(picture_title, end_date)
-    curr_webp_pic_down_load_path = month_dir_webp_path + "/curr.webp"
+    webp_pic_down_load_path = webp_path + "/{}.webp".format(webp_date)
     
     # 图片不存在->下载
     if not os.path.exists(pic_down_load_path):
@@ -52,10 +51,8 @@ def print_hi():
     # 图片格式转换为webp保存
     if not os.path.exists(webp_pic_down_load_path):
         im = Image.open(pic_down_load_path)
-        if not os.path.exists(webp_pic_down_load_path):
-            im.save(webp_pic_down_load_path,"webp")
-            im.save(curr_webp_pic_down_load_path,"webp")
-        print(curr_webp_pic_down_load_path, "图片格式转换为webp完成")
+        im.save(webp_pic_down_load_path,"webp"，quality=20)
+        print(webp_pic_down_load_path, "图片格式转换为webp完成")
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
