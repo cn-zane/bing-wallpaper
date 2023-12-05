0594c018cbd1fa84134c1dc68eceebb8e882d208
鲁湛湛 <cn_zane@outlook.com>
2023-12-05 10:28:02 +0800

    格式化代码

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime
 import json
 from PIL import Image
 
+
 def print_hi():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
@@ -34,7 +35,7 @@ def print_hi():
     # 月份路径(不存在则创建)
     if not os.path.exists(month_dir_path):
         os.makedirs(month_dir_path)
-        
+
     # webp月份路径(不存在则创建)
     if not os.path.exists(webp_path):
         os.makedirs(webp_path)
@@ -42,17 +43,18 @@ def print_hi():
     # 图片下载路径
     pic_down_load_path = month_dir_path + "/{}-{}.jpg".format(picture_title, end_date)
     webp_pic_down_load_path = webp_path + "/{}.webp".format(webp_date)
-    
+
     # 图片不存在->下载
     if not os.path.exists(pic_down_load_path):
         request.urlretrieve(url, pic_down_load_path)
         print(picture_title, "下载成功")
-        
+
     # 图片格式转换为webp保存
     if not os.path.exists(webp_pic_down_load_path):
         im = Image.open(pic_down_load_path)
-        im.save(webp_pic_down_load_path,"webp", quality=20)
+        im.save(webp_pic_down_load_path, "webp", quality=20)
         print(webp_pic_down_load_path, "图片格式转换为webp完成")
+
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
